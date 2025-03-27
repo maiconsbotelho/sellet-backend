31af1c8c218b3a29145cafc54268f5a470811cd4
maiconsbotelho <maicon.sbotelho@hotmail.com>
2025-03-27 15:58:19 -0300

    ajuste

--- servicos/views.py
+++ servicos/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from .models import Servico
 from .serializers import ServicoSerializer
 from usuarios.permissions import IsProfissionalOrAdmin, IsAdmin
